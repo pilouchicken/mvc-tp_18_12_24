a3f81b0f53effeeb44ce4afffbb169022dc7c0e1
pilouchicken <160750509+pilouchicken@users.noreply.github.com>
2024-12-18 16:19:51 +0100

    zzz

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once __DIR__ . '/../core/Database.php';
 require_once __DIR__ . '/../core/Router.php';
 require_once __DIR__ . '/../app/models/Task.php';
